3190987e8cf6db727004160f5da7e703fb6e3426
jackielu <jackielu@gmail.com>
2014-10-27 22:27:34 -0400

    Updated styling

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 //set an empty variable called selectedsq that can hold multiple values
 var selectedsq = [];
 var buttonid;
+var controlid;
 
 $(".box").click(function (){
-    // console.log(this);  //which class=box element did you click on?
+    console.log(this);  //which class=box element did you click on?
     selectedsq.push(this);  //add clicked element to array
-    // console.log(selectedsq);
-    $(selectedsq).css("border-color","aqua");  //indicate selection through aqua color
+    console.log(selectedsq);
+    $(selectedsq).css("border-color","aqua").css("color","aqua");  //indicate selection through aqua color
 })
 
 $("button").click(function()  //when a button is clicked
 {
     // console.log(this);  //what did you click on?
     buttonid=$(this).attr("id"); //get the ID value of the element you clicked on
-    // console.log(buttonid);  //print value of buttonid
+    console.log(buttonid);  //print value of buttonid
 
     if($(this).attr("id") === "unselect"){  //if the unselect button is clicked
-        $("div[class=box]").css("border-color", "black");//select all <div> with class "box", change border to black
+        $("li[class=box]").css("border-color", "white").css("color","white");//select all <div> with class "box", change border to black
         //$(selectedsq).css("border-color", "black");
         selectedsq = [];  //reset variable holding selectedsq
     };
